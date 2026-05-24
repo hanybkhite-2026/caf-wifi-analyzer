d13066403fc57b6d3040caf10d92cd9721a8d118
Hany Bkhite <hanybkhite@gmail.com>
2026-05-24 18:50:38 +0000

    import { initializeApp } from 'firebase/app'; import { getAuth } from 'f

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -11,5 +11,6 @@ export const firebaseConfig = {
   storageBucket: "caf-wifi-analyzer.firebasestorage.app",
   messagingSenderId: "101881422490",
   appId: "1:101881422490:web:d5f0dffdd4dc3fea6a7523",
+  measurementId: "G-SVF7CJY7J4",
   databaseURL: "https://caf-wifi-analyzer.firebaseio.com",
 };
